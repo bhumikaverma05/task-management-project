330fe11b303dd25aba9eb96a6013b9bca4000ba7
CoderYUI <bhaskar.23bey10012@vitbhopal.ac.in>
2025-11-11 19:06:31 +0530

    lead ka commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Backend running...");
+});
+
+app.listen(5000, () => console.log("Server started on port 5000"));
